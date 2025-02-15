6773f62d37913d119d22ab7af1a41e6a14501809
Tiandoudou <fridtjof.pohl@mac.com>
2025-02-15 20:40:27 +0100

    Update streamlit_app.py
    Session access is different from SiS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 st.title(":cup_with_straw: Customize your smoothie! :cup_with_straw:")
@@ -11,7 +10,9 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
-session = get_active_session()
+cnx = st. connection ("snowflake")
+session = cnx.session()
+
 my_dataframe = session.table ("smoothies.public.fruit_options").select(col ('FRUIT_NAME'))
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections = 5)
