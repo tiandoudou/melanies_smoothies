ba90d6371b6310edea00c330441893b2cf32b908
Tiandoudou <fridtjof.pohl@mac.com>
2025-02-15 21:06:16 +0100

    Update streamlit_app.py
    Fixed double =

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,4 +31,4 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sfdf = = st.dataframe (data=smoothiefroot_response.json(), use_container_width=True)
+sfdf = st.dataframe (data=smoothiefroot_response.json(), use_container_width=True)
